caa0f58d159b9d1902ceb7567851c105c883b957
chenjumin <chenjumin@163.com>
2019-09-12 22:54:11 +0800

    1

---
+++ springboot_project/src/main/java/com/seasy/Main.java
@@ -0,0 +1,19 @@
+package com.seasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class Main{
+	public static void main(String[] args){
+		SpringApplication.run(Main.class, args);
+	}
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+}
---
+++ springboot_project/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page session="true" pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>index</title>
+    </head>
+    <body>
+		hello world!		
+    </body>
+</html>
